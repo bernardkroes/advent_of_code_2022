e11be4aad95de8315a753a91248c06d8b76595c2
bernardkroes <bernard@shiftf5.nl>
2022-12-03 08:14:28 +0100

    Revisit first idea and use '&'

--- day_3.rb
+++ day_3.rb
@@ -1,32 +1,28 @@
 all_lines = File.read('day_3_input.txt').split("\n")
 # all_lines = File.read('day_3_test_input.txt').split("\n")
 
+def priority(el)
+  if ('a'..'z').include?(el)
+    return el.ord - 'a'.ord + 1
+  else 
+    return el.ord - 'A'.ord + 27
+  end
+end
+
 sum = 0
 all_lines.each do |line|
   ll = line.size / 2
   first_half = line[0..ll-1]
   second_half = line[ll..-1]
-  combined = first_half.split("").uniq + second_half.split("").uniq
-  duplicate = combined.select { |e| combined.count(e) > 1 }.uniq[0]
-  if ('a'..'z').include?(duplicate)
-    sum += duplicate.ord - 'a'.ord + 1
-  else 
-    sum += duplicate.ord - 'A'.ord + 27
-  end
+  combined = first_half.split("").uniq & second_half.split("").uniq
+  sum += priority(combined[0])
 end
 puts sum
 
 # part 2
 sum = 0
-(all_lines.size / 3).times do |i|
-  ll = i * 3
-  the_line = all_lines[ll].split("").uniq + all_lines[ll+1].split("").uniq + all_lines[ll+2].split("").uniq
-  badge = the_line.select { |e| the_line.count(e) == 3 }.uniq[0]
-  if ('a'..'z').include?(duplicate)
-    sum += badge.ord - 'a'.ord + 1
-  else 
-    sum += badge.ord - 'A'.ord + 27
-  end
+all_lines.each_slice(3) do |g|
+  combined = g[0].split("").uniq & g[1].split("").uniq & g[2].split("").uniq
+  sum += priority(combined[0])
 end
-
 puts sum
