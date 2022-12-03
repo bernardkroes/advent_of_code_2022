d8be6e01af4847a9912716d21fe94cf497ed3d23
bernardkroes <bernard@shiftf5.nl>
2022-12-03 14:55:51 +0100

    uniq is not needed

--- day_3.rb
+++ day_3.rb
@@ -11,7 +11,7 @@ sum = 0
 all_lines.each do |line|
   ll = line.size / 2
   first_half, second_half = line[0..ll-1], line[ll..-1]
-  combined = first_half.split("").uniq & second_half.split("").uniq
+  combined = first_half.split("") & second_half.split("")
   sum += priority(combined[0])
 end
 puts sum
@@ -19,8 +19,8 @@ puts sum
 # part 2
 sum = 0
 all_lines.each_slice(3) do |g|
-  # or:  combined = g[0].split("").uniq & g[1].split("").uniq & g[2].split("").uniq
-  combined = g.collect {|e| e.split("").uniq }.reduce(:&)
+  # or:  combined = g[0].split("") & g[1].split("") & g[2].split("")
+  combined = g.collect {|e| e.split("") }.reduce(:&)
   sum += priority(combined[0])
 end
 puts sum
