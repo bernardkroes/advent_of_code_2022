4d833aa275039ad17e3694be87a52403a85fd163
bernardkroes <bernard@shiftf5.nl>
2022-12-04 06:36:10 +0100

    Nicer?

--- day_4.rb
+++ day_4.rb
@@ -6,13 +6,22 @@ all_lines.each do |line|
   s1, s2 = line.split(",")
   s1_s, s1_e = s1.split("-").map(&:to_i)
   s2_s, s2_e = s2.split("-").map(&:to_i)
-  section1 = (s1_s..s1_e)
-  section2 = (s2_s..s2_e)
+  section1 = (s1_s..s1_e).to_a
+  section2 = (s2_s..s2_e).to_a
 
-  if section2.include?(s1_s) && section2.include?(s1_e) # or: section2.include?(section1.first) && section2.include?(section1.last)
-    the_count += 1
-  elsif section1.include?(s2_s) && section1.include?(s2_e)
-    the_count += 1
+# used this:
+#   section1 = (s1_s..s1_e)
+#   section2 = (s2_s..s2_e)
+#   if section2.include?(s1_s) && section2.include?(s1_e) # or: section2.include?(section1.first) && section2.include?(section1.last)
+#     the_count += 1
+#   elsif section1.include?(s2_s) && section1.include?(s2_e)
+#     the_count += 1
+#   end
+
+  # liked this more:
+  overlap = section1 & section2
+  if overlap == section1 || overlap == section2
+    the_count +=1
   end
 end
 puts the_count
@@ -22,13 +31,22 @@ all_lines.each do |line|
   s1, s2 = line.split(",")
   s1_s, s1_e = s1.split("-").map(&:to_i)
   s2_s, s2_e = s2.split("-").map(&:to_i)
-  section1 = (s1_s..s1_e)
-  section2 = (s2_s..s2_e)
+  section1 = (s1_s..s1_e).to_a
+  section2 = (s2_s..s2_e).to_a
 
-  if section2.include?(s1_s) || section2.include?(s1_e)
-    the_count += 1
-  elsif section1.include?(s2_s) || section1.include?(s2_e)
-    the_count += 1
+# used this:
+#   section1 = (s1_s..s1_e)
+#   section2 = (s2_s..s2_e)
+#   if section2.include?(s1_s) || section2.include?(s1_e)
+#     the_count += 1
+#   elsif section1.include?(s2_s) || section1.include?(s2_e)
+#     the_count += 1
+#   end
+
+  # liked this more:
+  overlap = section1 & section2
+  if overlap.count > 0
+    the_count +=1
   end
 end
 puts the_count
