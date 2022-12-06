8932410889207000d470273ec723f1c4f998fc8a
bernardkroes <bernard@shiftf5.nl>
2022-12-06 10:00:09 +0100

    added alternative

--- day_6.rb
+++ day_6.rb
@@ -3,10 +3,18 @@ chars = File.read('day_6_input.txt').split("")
 marker_length = 4      # use this line for part 2
 #marker_length = 14    # use this line for part 2
 
-start_pos = marker_length - 1
-chars[start_pos..-start_pos].each_with_index do |_, i|
-  if chars[i..(i+start_pos)].uniq.count == marker_length
+# end_pos = marker_length - 1
+# chars[0..-end_pos].each_with_index do |_, i|
+#   if chars[i..(i+end_pos)].uniq.count == marker_length
+#     puts i + marker_length
+#     exit
+#   end
+# end
+
+chars.each_cons(marker_length).with_index do |g, i|
+  if g.uniq.count == marker_length
     puts i + marker_length
     exit
   end
 end
+
