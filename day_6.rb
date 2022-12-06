a90ef6dc1a7439a4e07c05db386ae88a537cd2a9
bernardkroes <bernard@shiftf5.nl>
2022-12-06 10:37:48 +0100

    added alternative

--- day_6.rb
+++ day_6.rb
@@ -3,14 +3,6 @@ chars = File.read('day_6_input.txt').split("")
 marker_length = 4      # use this line for part 2
 #marker_length = 14    # use this line for part 2
 
-# end_pos = marker_length - 1
-# chars[0..-end_pos].each_with_index do |_, i|
-#   if chars[i..(i+end_pos)].uniq.count == marker_length
-#     puts i + marker_length
-#     exit
-#   end
-# end
-
 chars.each_cons(marker_length).with_index do |g, i|
   if g.uniq.count == marker_length
     puts i + marker_length
@@ -18,3 +10,11 @@ chars.each_cons(marker_length).with_index do |g, i|
   end
 end
 
+# or; (used this more or less:)
+# end_pos = marker_length - 1
+# chars[0..-end_pos].each_with_index do |_, i|
+#   if chars[i..(i+end_pos)].uniq.count == marker_length
+#     puts i + marker_length
+#     exit
+#   end
+# end
