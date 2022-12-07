ccada90f0c73e06008bf060a834ec4f1261050db
bernardkroes <bernard@shiftf5.nl>
2022-12-07 11:01:26 +0100

    A hash with a default value of zero

--- day_7.rb
+++ day_7.rb
@@ -1,20 +1,18 @@
 lines = File.read('day_7_input.txt').split("\n")
 # lines = File.read('day_7_test_input.txt').split("\n")
 
-sizes = {}
-dirs =["root"] # a stack of dirnames that we have followd
+sizes = Hash.new(0)
+dirs = ["root"] # a stack of dirnames that we have followed
 
-sizes[dirs.join("/")] = 0
 lines.each do |line|
   if line == "$ cd /"
-    dirs =["root"]
+    dirs = ["root"]
   elsif line.start_with?("$ cd ")
     if line == "$ cd .."
       dirs.pop
     else
       dirs.push(line.split.last)
     end
-    sizes[dirs.join("/")] ||= 0
   else # it is a file
     filesize = line.gsub(/[^0-9]*/,"").to_i
     dirs.each_with_index do |_,i|
