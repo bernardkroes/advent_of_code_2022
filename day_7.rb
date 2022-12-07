e7ce4cefd25d5ee30a90fae46a3b5cdb0d04cb2b
bernardkroes <bernard@shiftf5.nl>
2022-12-07 13:47:26 +0100

    Reworked some more

--- day_7.rb
+++ day_7.rb
@@ -2,7 +2,7 @@ lines = File.read('day_7_input.txt').split("\n")
 # lines = File.read('day_7_test_input.txt').split("\n")
 
 sizes = Hash.new(0)
-dirs = ["root"] # a stack of dirnames that we have followed
+dirs = ["root"] # a stack of current dirnames that we have followed
 
 lines.each do |line|
   if line == "$ cd /"
@@ -13,10 +13,10 @@ lines.each do |line|
     else
       dirs.push(line.split.last)
     end
-  else # it is a file
+  else # it is a file (or ls command that does not contain any digits)
     filesize = line.gsub(/[^0-9]*/,"").to_i
-    dirs.each_with_index do |_,i|
-      key = dirs[0..i].join("/")
+    1.upto(dirs.size) do |i|
+      key = dirs.first(i).join("/")
       sizes[key] += filesize
     end
   end
@@ -27,5 +27,5 @@ puts sizes.values.select { |v| v < 100000 }.sum
 
 # part 2
 unused_space = 70000000 - sizes["root"]
-required_extra_space = 30000000 - unused_space
+required_extra_space = 30000000 - unused_space # or: sizes["root"] - 40000000
 puts sizes.values.select { |v| v > required_extra_space }.min
