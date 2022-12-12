02de9b21e12a723e4585f11bdea6594f2d21eaea
bernardkroes <bernard@shiftf5.nl>
2022-12-12 07:34:58 +0100

    A bit cleaner

--- day_12.rb
+++ day_12.rb
@@ -1,6 +1,6 @@
 DELTA_MOVES = [[ 0,1], [-1,0], [ 1,0], [0,-1]]
 class Grid
-  attr_accessor :size_x, :size_y, :grid, :cur_x, :cur_y
+  attr_accessor :size_x, :size_y, :grid, :start_x, :start_y
 
   def initialize(in_lines)
     @grid = []
@@ -15,18 +15,18 @@ class Grid
         @dst_x, @dst_y = l.find_index("E"), y
       end
       if l.include?("S")
-        @cur_x, @cur_y = l.find_index("S"), y
+        @start_x, @start_y = l.find_index("S"), y
       end
     end
 
-    @grid[@cur_y][@cur_x] = 'a'
+    @grid[@start_y][@start_x] = 'a'
     @grid[@dst_y][@dst_x] = 'z'
   end
 
   def get_info
     puts "Size x: #{@size_x}"
     puts "Size y: #{@size_y}"
-    puts "Cur pos: #{@cur_x} #{@cur_y}"
+    puts "Cur pos: #{@start_x} #{@start_y}"
     puts "Dst pos: #{@dst_x} #{@dst_y}"
 
     @grid.each do |l|
@@ -50,20 +50,21 @@ class Grid
     the_moves
   end
 
-  def find_dst
+  def find_dst(start_positions)
     seen = {} # hash_key: x_y
 
     work_queue = []
     num_steps = 0
 
-    work_queue << [@cur_x, @cur_y, num_steps]
+    start_positions.each do |sp|
+      work_queue << [sp[0], sp[1], num_steps]
+    end
     while work_queue.size > 0
       the_state = work_queue.shift
 
       all_moves = possible_moves(the_state[0], the_state[1])
       all_moves.each do |m|
-        the_x = the_state[0] + m[0]
-        the_y = the_state[1] + m[1]
+        the_x, the_y = the_state[0] + m[0], the_state[1] + m[1]
         the_num_steps = the_state[2] + 1
         if (the_x == @dst_x) && (the_y == @dst_y)
           return the_num_steps
@@ -74,21 +75,16 @@ class Grid
         end
       end
     end
-    -1
   end
 
   def find_dst_part_2
-    min_num_steps = 10000
+    start_positions = []
     @grid.each_with_index do |l, y|
       l.each_with_index do |c, x|
-      if c == "a"
-        @cur_x, @cur_y = x, y
-        the_num_steps = find_dst
-        min_num_steps = the_num_steps if the_num_steps < min_num_steps && the_num_steps > 0
-      end
+        start_positions << [x, y] if c == "a"
       end
     end
-    return min_num_steps
+    find_dst(start_positions)
   end
 end
 
@@ -97,5 +93,5 @@ all_lines = File.read('day_12_input.txt').split("\n")
 
 g = Grid.new(all_lines)
 # g.get_info
-puts g.find_dst
+puts g.find_dst([[g.start_x, g.start_y]])
 puts g.find_dst_part_2
