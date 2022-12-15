8c9e9eeee6024ec59f3dc0332b37ccfc0345022b
bernardkroes <bernard@shiftf5.nl>
2022-12-15 09:16:13 +0100

    store distances with sensors

--- day_15.rb
+++ day_15.rb
@@ -1,32 +1,36 @@
 all_lines = File.read('day_15_input.txt').split("\n")
 # all_lines = File.read('day_15_test_input.txt').split("\n")
 
-sensors = []
+sensors = {}
 beacons = []
-dists = []
+
 all_lines.each do |line|
   all_ints = line.scan(/[0-9+\-]+/).map(&:to_i)
-  sensors << [all_ints[0], all_ints[1]]
+  sensor = [all_ints[0], all_ints[1]]
+  dist = (all_ints[0] - all_ints[2]).abs + (all_ints[1] - all_ints[3]).abs
+
+#  dists << (all_ints[0] - all_ints[2]).abs + (all_ints[1] - all_ints[3]).abs
+  sensors[sensor] = dist
   beacons << [all_ints[2], all_ints[3]]
-  dists << (all_ints[0] - all_ints[2]).abs + (all_ints[1] - all_ints[3]).abs
 end
 
-def is_covered?(sensors, dists, x, y)
-  sensors.each_with_index do |s,i|
-    return true if (s[0] - x).abs + (s[1] - y).abs <= dists[i]
+def is_covered?(sensors, x, y)
+  sensors.each do |s, dist|
+    return true if (s[0] - x).abs + (s[1] - y).abs <= dist
   end
   false
 end
 
-min_x, max_x = sensors.collect { |s| s[0]}.minmax
-max_d = dists.max
+min_x, max_x = sensors.keys.collect { |s| s[0]}.minmax
+max_d = sensors.values.max
 
 y = 2000000
 count = 0
 
 (min_x - max_d).upto(max_x + max_d) do |x|
-  if is_covered?(sensors, dists, x, y)
-    if !sensors.any? { |s| s[0] == x && s[1] == y} && !beacons.any? { |b| b[0] == x && b[1] == y}
+  if is_covered?(sensors, x, y)
+#    if !sensors.include?([x,y]) && !beacons.include?([x,y])
+    if !beacons.include?([x,y])
       count += 1
     end
   end
@@ -36,9 +40,10 @@ puts count
 # part 2: only search around the borders
 limit = 4000000
 
-sensors.each_with_index do |s,i|
+sensors.each do |s, dist|
   s_x, s_y = s[0], s[1]
-  the_check_dist = dists[i] + 1
+
+  the_check_dist = dist + 1
   (-the_check_dist).upto(the_check_dist) do |delta_x|
     delta_y = the_check_dist - delta_x
 
@@ -47,14 +52,14 @@ sensors.each_with_index do |s,i|
 
     the_y1 = s_y + delta_y
     next if the_y1 < 0 || the_y1 > limit
-    if !is_covered?(sensors, dists, the_x, the_y1)
+    if !is_covered?(sensors, the_x, the_y1)
       puts  the_x * 4000000 + the_y1
       exit
     end
 
     the_y2 = s_y - delta_y
     next if the_y2 < 0 || the_y2 > limit
-    if !is_covered?(sensors, dists, the_x, the_y2)
+    if !is_covered?(sensors, the_x, the_y2)
       puts the_x * 4000000 + the_y2
       exit
     end
