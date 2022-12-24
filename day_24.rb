1b248a1486fd78a667f42cf6c396ef03e4c53dac
bernardkroes <bernard@shiftf5.nl>
2022-12-24 12:57:12 +0100

    Day 24 clean up

--- day_24.rb
+++ day_24.rb
@@ -10,23 +10,20 @@ WAIT = 4
 
 grid = []
 bbs = [] # starting positions x,y, dir
+
 all_lines.each_with_index do |line, y|
   grid << []
   line.split("").each_with_index do |c, x|
-    if c == "#"
-      grid[y][x] = "#"
-    else
-      grid[y][x] = "."
+    grid[y][x] = (c == "#" ? "#" : ".")
+    the_dir = case c
+      when "<" then LEFT
+      when "^" then UP
+      when ">" then RIGHT
+      when "v" then DOWN
+      else
+        -1
     end
-    if c == "<"
-      bbs << [x, y, LEFT]
-    elsif c == "^"
-      bbs << [x, y, UP]
-    elsif c == ">"
-      bbs << [x, y, RIGHT]
-    elsif c == "v"
-      bbs << [x, y, DOWN]
-    end
+    bbs << [x,y,the_dir] if the_dir > -1
   end
 end
 size_x = grid[0].size
@@ -82,7 +79,6 @@ def possible_moves(grid, cur_x, cur_y)
   size_x = grid[0].size
   size_y = grid.size
 
-  # show_grid(grid, bbs_pos)
   mm = []
   DELTA_MOVES.each do |m|
     next_x, next_y = cur_x + m[0], cur_y + m[1]
@@ -95,18 +91,17 @@ end
 
 # calculate the grids for the first 1000 minutes
 grids = [Marshal.load(Marshal.dump(grid))]
-bbs_pos = Marshal.load(Marshal.dump(bbs))
 
 # first add the start grid!
 grid_for_this_minute = Marshal.load(Marshal.dump(grid))
-bbs_pos.each_with_index do |b, i|
+bbs.each_with_index do |b, i|
   grid_for_this_minute[b[1]][b[0]] = "*"
 end
 grids << grid_for_this_minute
 
 1000.times do |minute|
   grid_for_this_minute = Marshal.load(Marshal.dump(grid))
-  bbs_pos.each_with_index do |b, i|
+  bbs.each_with_index do |b, i|
     bx, by = b[0], b[1]
 
     bx = (bx + DELTA_MOVES[b[2]][0]) % size_x
@@ -115,7 +110,7 @@ grids << grid_for_this_minute
       bx = (bx + DELTA_MOVES[b[2]][0]) % size_x
       by = (by + DELTA_MOVES[b[2]][1]) % size_y
     end
-    bbs_pos[i] = [bx, by, b[2]]
+    bbs[i] = [bx, by, b[2]]
     grid_for_this_minute[by][bx] = "*"
   end
   grids << grid_for_this_minute
@@ -139,12 +134,13 @@ def shortest_path(grids, start_x, start_y, dst_x, dst_y, start_minute)
     if next_x == dst_x && next_y == dst_y
       shortest = the_minute if the_minute < shortest
     elsif the_minute < shortest
-      all_moves = possible_moves(grids[the_minute + 2], next_x, next_y, )
+      the_minute += 1
+      all_moves = possible_moves(grids[the_minute + 1], next_x, next_y, )
       all_moves.each do |next_m|
-        if !seen.has_key?([next_x, next_y, next_m, the_minute + 1])
-#          work_queue.unshift([next_x, next_y, next_m, the_minute + 1 ])        # use this for DFS
-          work_queue << [next_x, next_y, next_m, the_minute + 1 ]               # use this for BFS (faster for my input and implementation)
-          seen[[next_x, next_y, next_m, the_minute + 1]] = 1
+        if !seen.has_key?([next_x, next_y, next_m, the_minute])
+#          work_queue.unshift([next_x, next_y, next_m, the_minute + 1 ])    # use this for DFS
+          work_queue << [next_x, next_y, next_m, the_minute ]               # use this for BFS (faster for my input and implementation)
+          seen[[next_x, next_y, next_m, the_minute]] = 1
         end
       end
     end
