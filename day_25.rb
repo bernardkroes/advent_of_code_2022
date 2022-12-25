898e7b13b7a0330507272707a82c0c3c8d9e4879
bernardkroes <bernard@shiftf5.nl>
2022-12-25 11:08:18 +0100

    Moar solutions

--- day_25.rb
+++ day_25.rb
@@ -1,23 +1,38 @@
 all_lines = File.read('day_25_input.txt').split("\n")
 # all_lines = File.read('day_25_test_input.txt').split("\n")
 
-SVALS = { "=" => -2, "-" => -1, "0" => 0, "1" => 1, "2" => 2 }
+# solution based on base 5 conversions:
 
 def from_snafu(line)
-  temp = 0
-  base = 1
-  line.split("").reverse.each_with_index do |c,i|
-    val = SVALS[c]
-    temp += val * base
-    base *= 5
-  end
-  return temp
+  num_b5_slen = line.size
+  tr_line = line.tr('=\-012','01234') # in this conversion we have added "22222", we need to subtract that
+  num_b5 = tr_line.to_i(5)
+  delta = ("2" * num_b5_slen).to_i(5)
+  return num_b5 - delta
+end
+
+# just the other way around
+def to_snafu(num)
+  num_b5_slen = num.to_s(5).size
+  delta = ("2" * num_b5_slen).to_i(5) # in the tr below we will subtract "222222", add it (converted) first
+  num_b5_s = (delta + num).to_s(5)
+  return num_b5_s.tr('01234', '=\-012')
 end
 
 sum = 0
 all_lines.each do |line|
-  sum += from_snafu(line)
+  val = from_snafu(line)
+  sum += val
 end
+
+puts sum
+puts to_snafu(sum)
+puts from_snafu(to_snafu(sum)) # verify
+
+__END__
+
+# first solution was to do the to_snafu manually (and submit)
+# second solution was based on code by jonathanpaulson
 
 def max_value(p5)
   return 2 if p5 == 1
@@ -44,8 +59,7 @@ puts to_snafu(the_num,pp)
 
 __END__
 
-34061028947237
-puts from_snafu("2-0=11=-0-2-1==1=-22")
+puts from_snafu("2-0=11=-0-2-1==1=-22") # verify => 34061028947237
 
  Decimal          SNAFU
         1              1
@@ -64,18 +78,18 @@ puts from_snafu("2-0=11=-0-2-1==1=-22")
     12345        1-0---0
 314159265  1121-1110-1=0
 
-1=-0-2
-12111
-2=0=
-21
-2=01
-111
-20012
-112
-1=-1=
-1-12
-12
-1=
-122
+ SNAFU  Decimal
+1=-0-2     1747
+ 12111      906
+  2=0=      198
+    21       11
+  2=01      201
+   111       31
+ 20012     1257
+   112       32
+ 1=-1=      353
+  1-12      107
+    12        7
+    1=        3
+   122       37
 
-34061028947237
